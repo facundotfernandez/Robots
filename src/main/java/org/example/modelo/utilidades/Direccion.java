48b04a4327854c7b8d22269743a99b892c7c0f06
facundotfernandez <ftfernandez@fi.uba.ar>
2024-04-13 19:56:54 -0300

    Direccion movido a utilidades

--- src/main/java/org/example/modelo/utilidades/Direccion.java
+++ src/main/java/org/example/modelo/utilidades/Direccion.java
@@ -1,22 +1,27 @@
-package org.example.modelo.unidades;
+package org.example.modelo.utilidades;
 
 public enum Direccion {
-    ARRIBA(new int[]{-1, 0}),
-    ABAJO(new int[]{1, 0}),
-    IZQUIERDA(new int[]{0, -1}),
-    DERECHA(new int[]{0, 1}),
-    ARRIBA_IZQ(new int[]{-1, -1}),
-    ARRIBA_DER(new int[]{-1, 1}),
-    ABAJO_IZQ(new int[]{1, -1}),
-    ABAJO_DER(new int[]{1, 1});
+    ARRIBA(-1, 0), ABAJO(1, 0), IZQUIERDA(0, -1), DERECHA(0, 1), ARRIBA_IZQ(-1, -1), ARRIBA_DER(-1, 1), ABAJO_IZQ(1, -1), ABAJO_DER(1, 1);
 
-    private final int[] cambio;
+    private final int[] direccion;
 
-    Direccion(int[] cambio) {
-        this.cambio = cambio;
+    Direccion(int fila, int columna) {
+        this.direccion = new int[]{fila, columna};
     }
 
-    public int[] getCambio() {
-        return cambio;
+    public static Direccion getDireccion(int i) {
+        return Direccion.values()[i];
+    }
+
+    public int[] getDireccion() {
+        return direccion;
+    }
+
+    public int getFila() {
+        return direccion[0];
+    }
+
+    public int getColumna() {
+        return direccion[1];
     }
 }
