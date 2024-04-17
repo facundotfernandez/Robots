ab51d175d904c4df8e8abf595cac8f6842796f4c
facundotfernandez <ftfernandez@fi.uba.ar>
2024-04-17 02:00:21 -0300

    Avance implementacion inicio de juego y principio de movimientos

--- src/main/java/org/example/modelo/jugabilidad/Juego.java
+++ src/main/java/org/example/modelo/jugabilidad/Juego.java
@@ -1,44 +1,23 @@
 package org.example.modelo.jugabilidad;
 
-import org.example.modelo.tablero.*;
+import org.example.modelo.tablero.CeldaInvalidaException;
 import org.example.modelo.unidades.Jugador;
 import org.example.modelo.unidades.Personaje;
 import org.example.modelo.unidades.Robot;
 import org.example.modelo.utilidades.Direccion;
 
-import java.util.LinkedList;
-import java.util.Random;
-
 public class Juego {
-    private final Tablero2<Personaje> tablero;
-    private final int EXPLOSION = 0;
-    private LinkedList<Celda<Personaje>> celdasDeRobots;
-    private Celda<Personaje> ubicacionJugador;
-    private Nivel nivel_act;
+    private final Jugador jugador;
+    private Nivel nivel;
 
-    public Juego(int fila, int columna, String nombreJugador, int tpsSeguros) throws CeldaInvalidaException {
-        this.tablero = new Tablero2<Personaje>(fila, columna);
-        this.nivel_act = new Nivel(1);
-        iniciarJuego(nombreJugador,tpsSeguros);
-
-    }
-
-    private void actualizarJuego() throws CeldaInvalidaException {
-        for (Celda<Personaje> celda : celdasDeRobots) {
-            try {
-                mover(celda, Direccion.getDireccion(new Random().nextInt(Direccion.values().length)));
-            } catch (CeldaInvalidaException e) {
-                mover(celda, Direccion.getDireccion(new Random().nextInt(Direccion.values().length)));
-            }
-        }
+    public Juego(int filas, int columnas, String nombreJugador, int tpsSeguros) throws CeldaInvalidaException {
+        this.nivel = new Nivel(1);
+        this.jugador = new Jugador(nombreJugador, tpsSeguros);
+        iniciarJuego(nombreJugador, tpsSeguros);
     }
 
     private void avanzarNivel() {
-        this.nivel_act = new Nivel(nivel_act.getId() + 1);
-    }
-
-    private void mover(Celda<Personaje> origen, Direccion direccion) throws CeldaInvalidaException {
-        mover(origen, tablero.getCelda(origen.getX() + direccion.getFila(), origen.getY() + direccion.getColumna()));
+        this.nivel = new Nivel(nivel.getId() + 1);
     }
 
     private boolean esRobot(Personaje personaje) {
@@ -49,7 +28,7 @@ public class Juego {
         return personaje.getClass() == Jugador.class;
     }
 
-    private void mover(Celda<Personaje> origen, Celda<Personaje> destino) throws CeldaInvalidaException {
+    /*private void mover(Celda<Personaje> origen, Celda<Personaje> destino) throws CeldaInvalidaException {
         Personaje ocupanteOrigen = origen.vaciar();
         try {
             destino.ocupar(ocupanteOrigen);
@@ -65,8 +44,7 @@ public class Juego {
                     destino.ocupar(ocupanteOrigen);
                 }
             }
-        }
-        if (esRobot(destino.getOcupante())) {
+        } if (esRobot(destino.getOcupante())) {
             celdasDeRobots.add(destino);
         } else if (esJugador(destino.getOcupante())) {
             ubicacionJugador = destino;
@@ -82,8 +60,7 @@ public class Juego {
             Personaje ocupanteOrigen = origen.getOcupante();
             Personaje ocupanteDestino = destino.getOcupante();
 
-            if ((esJugador(ocupanteOrigen) && esRobot(ocupanteDestino)) ||
-                    (esJugador(ocupanteDestino) && esRobot(ocupanteOrigen))) {
+            if ((esJugador(ocupanteOrigen) && esRobot(ocupanteDestino)) || (esJugador(ocupanteDestino) && esRobot(ocupanteOrigen))) {
                 juegoFinalizado();
                 return;
             }
@@ -100,18 +77,25 @@ public class Juego {
             }
 
         }
-    }
+    }*/
 
     private void juegoFinalizado() {
         System.out.println("Juego finalizado");
     }
-    private void iniciarJuego(String nombreJugador, int tpsSeguros) throws CeldaInvalidaException {
-        tablero.ocuparCeldaCentral(new Jugador(nombreJugador, tpsSeguros));
-        this.ubicacionJugador = tablero.getCeldaCentral();
 
-        this.celdasDeRobots = new LinkedList<>();
-        for (Robot robot : nivel_act.getRobots()) {
-            Celda<Personaje> celdaRobot = tablero.ocuparCeldaRandom(robot);
-            celdasDeRobots.add(celdaRobot);
+    private void iniciarJuego(String nombreJugador, int tpsSeguros) throws CeldaInvalidaException {
+        nivel.ubicarEnCentro(jugador);
+
+        for (Robot robot : nivel.getRobots()) {
+            nivel.ubicar(robot);
+        }
+    }
+
+    private void jugarTurno(Jugador jugador, int[] direccionJugador) {
+        var direccion = Direccion.getDireccion(direccionJugador);
+        nivel.mover(jugador.getUbicacion(), direccion);
+        jugador.mover(direccionJugador);
+
+        nivel.moverRobots(jugador.getUbicacion());
     }
 }
--- src/main/java/org/example/modelo/jugabilidad/Nivel.java
+++ src/main/java/org/example/modelo/jugabilidad/Nivel.java
@@ -1,5 +1,8 @@
 package org.example.modelo.jugabilidad;
 
+import org.example.modelo.tablero.Punto;
+import org.example.modelo.tablero.Tablero;
+import org.example.modelo.unidades.Personaje;
 import org.example.modelo.unidades.Robot;
 
 import java.util.LinkedList;
@@ -9,10 +12,14 @@ public class Nivel {
 
     private final LinkedList<Robot> robots;
     private final int id;
+    private final Tablero<Personaje> tablero;
+
 
     public Nivel(int id) {
         this.id = id;
-        this.robots = createRobots(id * 3);
+        var cantRobots = id * 3;
+        this.robots = createRobots(cantRobots);
+        this.tablero = new Tablero<>(cantRobots * 2, cantRobots * 2);
     }
 
     public int getId() {
@@ -26,8 +33,31 @@ public class Nivel {
     public LinkedList<Robot> createRobots(int cantidad) {
         LinkedList<Robot> listado = new LinkedList<>();
         for (int i = 0; i < cantidad; i++) {
-            listado.add(new Robot(new Random().nextInt(3)));
+            listado.add(new Robot(new Random().nextInt(2) + 1));
         }
         return listado;
+    }
+
+    public Tablero<Personaje> getTablero() {
+        return tablero;
+    }
+
+    public void ubicar(Personaje ocupante) {
+        tablero.ubicar(ocupante);
+    }
+
+    public void ubicar(Personaje ocupante, int fila, int columna) {
+        tablero.ubicar(ocupante, fila, columna);
+    }
+
+    public void ubicarEnCentro(Personaje ocupante) {
+        tablero.ubicarEnCentro(ocupante);
+        ocupante.mover(tablero.getFilas() / 2, tablero.getColumnas() / 2);
+    }
+
+    public void mover(Punto ubicacion, int direccion) {
+    }
+
+    public void moverRobots(Punto ubicacion) {
     }
 }
