2eec6ee9587b3d26e0a7f05dba621a36f38128d1
facundotfernandez <ftfernandez@fi.uba.ar>
2024-04-18 14:34:17 -0300

    Hotfix - Dificultades ahora pertenecen a [2,4]

--- src/main/java/org/example/modelo/utilidades/Constantes.java
+++ src/main/java/org/example/modelo/utilidades/Constantes.java
@@ -1,8 +1,8 @@
 package org.example.modelo.utilidades;
 
 public class Constantes {
-    public static final int FACIL = 1;
-    public static final int DIFICIL = 3;
+    public static final int FACIL = 2;
+    public static final int DIFICIL = 4;
     public static final int INCENDIO = 0;
     public static final int TIPOS_ROBOTS = 3;
     public static final int MULTIPLICADOR_PUNTAJE = 10;
